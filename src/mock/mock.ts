abf8fbf2a4f2d51491aabf5ac4d1d2089422e5bd
Roberto <cacaracteres86@gmail.com>
2022-11-19 21:40:42 +0100

    Avance en test robots controller

---
+++ src/mock/mock.ts
@@ -0,0 +1,17 @@
+import { Robot } from '../entities/robot.js';
+export const mockData: Array<Robot> = [
+    {
+        name: 'Walle',
+        image: 'url',
+        speed: 5,
+        endurance: 8,
+        date: 'date string',
+    },
+    {
+        name: 'eva',
+        image: 'new url ',
+        speed: 4,
+        endurance: 5,
+        date: 'new date string',
+    },
+];
