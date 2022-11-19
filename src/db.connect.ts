491a0187100c25238c60ebc7ef8e7babf52da8ee
Roberto <cacaracteres86@gmail.com>
2022-11-19 16:46:42 +0100

    probando sonars

--- src/db.connect.ts
+++ src/db.connect.ts
@@ -5,6 +5,7 @@ export function dbConnect() {
     const DBName =
         process.env.NODE_ENV !== 'test' ? 'RobertoDocuments' : 'CodersTesting';
     const uri = `mongodb+srv://${USER}:${PASSWORD}@cluster0.yswmjv0.mongodb.net/${DBName}?retryWrites=true&w=majority`;
-
+    console.log({ DBName });
+    console.log({ uri });
     return mongoose.connect(uri);
 }
