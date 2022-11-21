a89e6de2f881c71eb7fbe83897dafae102da62e3
Roberto <cacaracteres86@gmail.com>
2022-11-21 18:59:43 +0100

    probando que devuelva el token

--- src/data/dataUser.ts
+++ src/data/dataUser.ts
@@ -1,12 +1,6 @@
 export type id = number | string;
 
-export interface Data<T> {
-    getAll: () => Promise<Array<T>>;
-    get: (id: id) => Promise<T>;
-    post: (data: Partial<T>) => Promise<T>;
-    patch: (id: id, data: Partial<T>) => Promise<T>;
-    delete: (id: id) => Promise<{ id: id }>;
-}
+// Interface Segregation
 
 export interface BasicRepo<T> {
     get: (id: id) => Promise<T>;
